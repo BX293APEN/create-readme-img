c44080c4f3ff81e4e6e9d9a2c79849ae305c0dce
PEN <uemasaki20011222@gmail.com>
2025-12-06 19:50:07 +0900

    init

--- scripts/capture.js
+++ scripts/capture.js
@@ -2,14 +2,14 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
-  // HTMLを直接読み込む
   const html = fs.readFileSync('./img/img.html', 'utf-8');
   await page.setContent(html);
 
-  // スクリーンショットを保存
   await page.screenshot({ path: './assets/img.png' });
 
   await browser.close();
